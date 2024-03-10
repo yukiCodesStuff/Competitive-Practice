9e38d17e10918253d6327cac52e011bfef054bf7
yukiCodesStuff <yuki.janvier@gmail.com>
2024-03-09 23:32:42 -0600

    setup for trick or treat

---
+++ set7/trickOrTreat.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+double dist(double num) {
+
+}
+
+int main() {
+    int n;
+    while (cin >> n) {
+        if (n == 0) break;
+
+        // keep trying values until within threshold
+
+        for (int i = 0; i < n; ++i) {
+            double x, y;
+            cin >> x >> y;
+
+        }
+    }
+    return 0;
+}
