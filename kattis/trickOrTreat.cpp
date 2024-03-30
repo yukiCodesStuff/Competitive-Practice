660f85f6eaa086420421ee8c0c6ae57d32f2aede
yukiCodesStuff <yuki.janvier@gmail.com>
2024-03-29 20:09:07 -0500

    got trick or treat

--- kattis/trickOrTreat.cpp
+++ kattis/trickOrTreat.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
 #include <utility>
 #include <vector>
+#include <cmath>
+#include <math.h>
+#include <limits>
 
 using namespace std;
 
-double dist(double num) {
+typedef vector<pair<double, double>> vpff;
 
+double calcTime(double x, vpff &houses) {
+    double maxTime = 0;
+    for (pair<double, double> &coord : houses) {
+        double dist = sqrt(pow(x - coord.first, 2) + pow(coord.second, 2));
+        maxTime = max(maxTime, dist);
+    }
+    return maxTime;
 }
 
 int main() {
@@ -13,13 +23,31 @@ int main() {
     while (cin >> n) {
         if (n == 0) break;
 
-        // keep trying values until within threshold
-
+        vpff houses(n);
         for (int i = 0; i < n; ++i) {
-            double x, y;
-            cin >> x >> y;
-
+            double x, y; cin >> x >> y;
+            houses[i] = make_pair(x, y);
         }
+
+        double left = -200000, right = 200000;
+        int itrCt = 0;
+        double epsilon = 1e-7;
+        while (abs(right - left) > epsilon) {
+            double mid1 = left + ((right - left) / 3);
+            double mid2 = right - ((right - left) / 3);
+
+            double mid1Dist = calcTime(mid1, houses);
+            double mid2Dist = calcTime(mid2, houses);
+            if (mid1Dist < mid2Dist) {
+                right = mid2;
+            } else {
+                left = mid1;
+            }
+
+            itrCt++;
+        }
+
+        cout << (left + right) / 2 << " " << calcTime((left + right) / 2, houses) << endl;;
     }
     return 0;
 }
