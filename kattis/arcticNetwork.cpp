a989cab4b642e765fbc49ad86ad554aa8f6d2fe5
yukiCodesStuff <yuki.janvier@gmail.com>
2024-04-03 14:03:48 -0500

    daily LC word search

---
+++ kattis/arcticNetwork.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+int main() {
+    int n; cin >> n;
+    while (n--) {
+        int s, p; cin >> s >> p;
+        while (p--) {
+            int x, y; cin >> x >> y;
+        }
+    }
+    return 0;
+}
