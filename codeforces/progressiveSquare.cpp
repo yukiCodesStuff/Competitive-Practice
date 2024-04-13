4e7fff66e30f17e5960074105a2e6addeb2dcec6
yukiCodesStuff <yuki.janvier@gmail.com>
2024-04-13 12:30:35 -0500

    progressive squares

--- codeforces/progressiveSquare.cpp
+++ codeforces/progressiveSquare.cpp
@@ -8,46 +8,36 @@ using namespace std;
 int main() {
     int t; cin >> t;
     while (t--) {
-        int n, c, d; cin >> n >> c >> d;
-        unordered_map<long long, int> numCt;
-        long long currMin = 1e18;
+        long long n, c, d; cin >> n >> c >> d;
+        vector<long long> a(n * n);
         for (int i = 0; i < n * n; ++i) {
-            long long num; cin >> num;
-            numCt[num]++;
-            currMin = min(currMin, num);
+            cin >> a[i];
         }
-
-        vector<vector<long long>> matrix(n, vector<long long>(n));
-        matrix[0][0] = currMin;
+        sort(a.begin(), a.end());   
+        vector<long long> b(n * n);
+        b[0] = a[0];
+        for (int i = n; i < n * n; i += n) {
+            b[i] = b[i - n] + c;
+        }
         for (int i = 0; i < n; ++i) {
-            if (i > 0) matrix[i][0] = matrix[i - 1][0] + c;
             for (int j = 1; j < n; ++j) {
-                matrix[i][j] = matrix[i][j - 1] + d;
+                b[(i * n) + j] = b[(i * n) + j - 1] + d;
+                // cout << j << " " << (i * n) + j - 1 << endl;
             }
         }
+        sort(b.begin(), b.end());
 
+        // for (auto i : a) {
+        //     cout << i << " ";
+        // }
         // cout << endl;
-        // for (auto r : matrix) {
-        //     for (auto i : r) {
-        //         cout << i << " ";
-        //     }
-        //     cout << endl;
+        // for (auto i : b) {
+        //     cout << i << " ";
         // }
         // cout << endl;
 
-        bool no = false;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 1; j < n; ++j) {
-                if (!numCt[matrix[i][j]]) {
-                    cout << "NO" << endl;
-                    no = true;
-                    break;
-                } else numCt[matrix[i][j]]--;
-            }
-        }
-
-        if (!no) cout << "YES" << endl;
-
+        if (a == b) cout << "YES" << endl;
+        else cout << "NO" << endl;
     }
     return 0;
 }
